e94b859339228d0846dca7f5cbd31721beb95592
MOHD UMAIR <106958647+MohdUmair122@users.noreply.github.com>
2023-08-30 15:02:08 +0530

    Add files via upload

---
+++ java_datatypes.java
@@ -0,0 +1,14 @@
+public class java_datatypes {
+    public static void main(String args[]){
+        //byte
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean v = false;
+        double p = 10.8;
+        // for storing very big floating number than we use 'double' instead of 'float'
+        int n = 56;
+        // for storing very big integer number than we use 'long' instead of 'int'
+    }
+}
---
+++ javabasics.java
@@ -0,0 +1,16 @@
+public class javabasics{
+    public static void main(String args[]){
+        // Output in java
+        System.out.print("HELLO UMAIR!!\n");
+        // both functions are same
+        // 'println' aur last m '\n'
+        System.out.println("HELLO UMAIR ji!!");
+        System.out.println("HELLO UMAIR sirji!!");
+
+        // Printing pattern
+        System.out.print("****\n");
+        System.out.print("***\n");
+        System.out.print("**\n");
+        System.out.print("*\n");
+    }
+}
