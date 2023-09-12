5f19febfc00f0de9d6f12d126f3f0df65a9b9c9f
MOHD UMAIR <106958647+MohdUmair122@users.noreply.github.com>
2023-09-13 00:10:13 +0530

    Add files via upload

---
+++ 6, All about Array's/creation_of_array.java
@@ -0,0 +1,17 @@
+public class creation_of_array {
+    public static void main(String args[]){
+        // Now we gonna learn
+        // How to create an array
+        //  { "datatype arrayname[] = new datatype[size]"
+        // Creating an array
+        // Array is immutable
+
+        // by entering a size
+        int marks[] = new int[50];
+
+        // by entring a value's
+        int numbers[] = {1,2,3,4,5,6,7};
+        // one more example
+        String fuits[] = {"apple","mango","banana","orange"};
+     }
+    }
